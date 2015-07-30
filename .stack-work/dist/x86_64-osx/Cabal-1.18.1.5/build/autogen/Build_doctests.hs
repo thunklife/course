2f73ee56506744f105e99fd65d033e118d8fde1a
Jesse Williamson <jesse@reflexionhealth.com>
2015-07-30 08:54:03 -0700

    chore(stack): Convert to stack
    Cabal was giving me shit.

---
+++ .stack-work/dist/x86_64-osx/Cabal-1.18.1.5/build/autogen/Build_doctests.hs
@@ -0,0 +1,3 @@
+module Build_doctests where
+deps :: [String]
+deps = ["QuickCheck-2.7.6","array-0.5.0.0","base-4.7.0.2","containers-0.5.5.1","doctest-0.9.13","directory-1.2.1.0","filepath-1.3.0.2","template-haskell-2.9.0.0"]
